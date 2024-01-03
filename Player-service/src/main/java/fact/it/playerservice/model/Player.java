24c17b4174bf0da15cd9c3d3680700a90011f0d7
sebastian-vangrieken <r0889358@student.thomasmore.be>
2024-01-03 20:59:06 +0100

    changed routes in api-gateway

--- Player-service/src/main/java/fact/it/playerservice/model/Player.java
+++ Player-service/src/main/java/fact/it/playerservice/model/Player.java
@@ -10,7 +10,7 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Player {
+public class  Player {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
